e30329afa06afc75c06433c9220cc02831ef24ba
axrasp <aaraspopin@gmail.com>
2022-12-07 13:01:16 +0300

    templates added

--- project/urls.py
+++ project/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
+
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', render, kwargs={'template_name': 'index.html'}, name='index'),
+    path('service/', render, kwargs={'template_name': 'service.html'}, name='service'),
+    path('service/reservation', render, kwargs={'template_name': 'reservation.html'}, name='reservation'),
+    path('accounts/profile/', render, kwargs={'template_name': 'profile.html'}, name='profile'),
+    path('accounts/login/', render, kwargs={'template_name': 'service.html'}, name='login')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
