47149f744f49ce52c88314f4b8bd5f5f24d8f71e
CaDiBob <nikolay.v.byankin@gmail.com>
2022-12-07 17:35:06 +0900

    added base admin model

--- beauty_salon/admin.py
+++ beauty_salon/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Order,
+    Salon,
+    Schedule,
+    Specialization,
+    Worker,
+    Procedure
+)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Procedure)
+class ProcedureAdmin(admin.ModelAdmin):
+    pass
